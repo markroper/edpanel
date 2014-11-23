8acdc322e5c2d38038335e0fa4cdd5296ade2ebb
markroper <markroper@gmail.com>
2014-11-23 10:34:09 -0500

    Adds an assignments API controller endpoint, removes the boilerplate endpoint. Up next: endpoint test cases and making use of the ErrorCode object to generate valid ResponseEntities both in the happy path cases and the degenerate cases.

---
+++ warehouse/src/main/java/com/scholarscore/api/controller/AssignmentController.java
@@ -0,0 +1,69 @@
+package com.scholarscore.api.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.scholarscore.models.Assignment;
+import com.scholarscore.api.util.ErrorCode;
+
+@Controller
+@RequestMapping("/api/v1/assignment")
+public class AssignmentController {
+	//TODO: @mroper we need to add a real persistence layer that we call instead of manipulating this map
+	public static final String JSON_ACCEPT_HEADER = "application/json";
+	public static Map<Long, Assignment> assignments = new HashMap<>();
+	public static Long nextAssignmentId = 0l;
+
+	 @RequestMapping(value = "/{assignmentId}", method = RequestMethod.GET, produces = {JSON_ACCEPT_HEADER})
+	 @SuppressWarnings("rawtypes")
+	 public @ResponseBody ResponseEntity getAssignment(
+			 @PathVariable(value="assignmentId") Long assignmentId) {
+		 return new ResponseEntity<Assignment>(assignments.get(assignmentId), HttpStatus.OK);
+
+	 }
+
+	 @RequestMapping(value = "/", method = RequestMethod.POST, produces = {JSON_ACCEPT_HEADER})
+	 @SuppressWarnings("rawtypes")
+	 public @ResponseBody ResponseEntity createAssignment(@RequestBody Assignment assignment) {
+		 assignment.setId(nextAssignmentId++);
+		 assignments.put(assignment.getId(), assignment);
+		 return new ResponseEntity<Assignment>(assignment, HttpStatus.OK);
+	 }
+	 
+	 @RequestMapping(value = "/{assignmentId}", method = RequestMethod.PUT, produces = {JSON_ACCEPT_HEADER})
+	 @SuppressWarnings("rawtypes")
+	 public @ResponseBody ResponseEntity replaceAssignment(
+			 @PathVariable(value="assignmentId") Long assignmentId,
+			 @RequestBody Assignment assignment) {
+		 ResponseEntity<Assignment> returnValue = null;
+		 if(null != assignmentId && assignments.containsKey(assignmentId)) {
+			 returnValue = new ResponseEntity<Assignment>(assignments.get(assignmentId), HttpStatus.OK);
+		 } else {
+			 returnValue = new ResponseEntity<Assignment>((Assignment)null, HttpStatus.NOT_FOUND);
+		 }
+		 return returnValue;
+	 }
+	 
+	 @RequestMapping(value = "/{assignmentId}", method = RequestMethod.DELETE, produces = {JSON_ACCEPT_HEADER})
+	 @SuppressWarnings("rawtypes")
+	 public @ResponseBody ResponseEntity deleteAssignment(
+			 @PathVariable(value="assignmentId") Long assignmentId) {
+		 HttpStatus status = HttpStatus.OK;
+		 if(null == assignmentId) {
+			 status = HttpStatus.BAD_REQUEST;
+		 } else if (!assignments.containsKey(assignmentId)) {
+			 status = HttpStatus.NOT_FOUND;
+		 }
+		 assignments.remove(assignmentId);
+		 return new ResponseEntity<Assignment>((Assignment) null, status);
+	 }
+}
