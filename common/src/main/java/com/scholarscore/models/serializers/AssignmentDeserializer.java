19d2938e475ff71a4b080533fafaf1d8a3862af7
jordan <jodamn AT gmail>
2014-11-30 20:39:40 -0500

    cleaned up imports

--- common/src/main/java/com/scholarscore/models/serializers/AssignmentDeserializer.java
+++ common/src/main/java/com/scholarscore/models/serializers/AssignmentDeserializer.java
@@ -2,11 +2,7 @@ package com.scholarscore.models.serializers;
 
 import java.io.IOException;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.scholarscore.models.Assignment;
 
