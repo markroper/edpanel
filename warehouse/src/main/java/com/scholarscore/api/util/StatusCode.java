3a3d8278cd04c8e34b6c52da32038fe4496fb027
jordan <jodamn AT gmail>
2015-01-02 21:35:55 -0500

    just reformatting whitespace (changing mix of tabs and spaces to just spaces)

--- warehouse/src/main/java/com/scholarscore/api/util/StatusCode.java
+++ warehouse/src/main/java/com/scholarscore/api/util/StatusCode.java
@@ -10,63 +10,62 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
- * The ErrorCode object returned by the API in those cases when the 
- * the expected object is not available due to a 404, 500 or other degenerate case.  
- * It encapsulates a ScholarScore error code, a human readable message, and an 
+ * The ErrorCode object returned by the API in those cases when the
+ * the expected object is not available due to a 404, 500 or other degenerate case.
+ * It encapsulates a ScholarScore error code, a human readable message, and an
  * HttpStatus code which is not serialized as it is redundant to the HttpStatus that
  * will be in the actual http API response to the client.
- * 
- * @author markroper
  *
+ * @author markroper
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties( {"httpStatus"} )
+@JsonIgnoreProperties({"httpStatus"})
 public class StatusCode {
-	private int code;
-	private String message;
-	private Object[] arguments;
-	
-	public StatusCode() {
-		
-	}
-	
-	public StatusCode(int code, String msg) {
-	    this.code = code;
-	    this.message = msg;
-	}
-	
-	public StatusCode(StatusCode code) {
-	    this.code = code.code;
-	    this.message = code.message;
-	    this.arguments = code.arguments;
-	}
-	
-   public StatusCode(StatusCode code, Object[] args) {
+    private int code;
+    private String message;
+    private Object[] arguments;
+
+    public StatusCode() {
+
+    }
+
+    public StatusCode(int code, String msg) {
+        this.code = code;
+        this.message = msg;
+    }
+
+    public StatusCode(StatusCode code) {
+        this.code = code.code;
+        this.message = code.message;
+        this.arguments = code.arguments;
+    }
+
+    public StatusCode(StatusCode code, Object[] args) {
         this.code = code.code;
         this.message = code.message;
         this.arguments = args;
     }
 
-	public int getCode() {
-		return code;
-	}
+    public int getCode() {
+        return code;
+    }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
     public Object[] getArguments() {
-        if(null != arguments) {
+        if (null != arguments) {
             return arguments.clone();
         } else {
             return null;
@@ -74,13 +73,12 @@ public class StatusCode {
     }
 
     public void setArguments(Object[] args) {
-        if(null == args) {
+        if (null == args) {
             this.arguments = null;
         } else {
             this.arguments = Arrays.copyOf(args, args.length);
         }
     }
-    
-    
-	
+
+
 }
