ad7ebbf396c39ef4fc41795e0894f3423cceec44
jordan <jodamn AT gmail>
2015-01-04 19:13:39 -0500

    repopulate the db schema after cleaning it out

--- database/createMySqlDatabase.sql
+++ database/createMySqlDatabase.sql
@@ -1,3 +1,4 @@
+-- currently not used automatically, run manually the first time to provision the user
 CREATE DATABASE scholar_warehouse;
 CREATE USER 'warehouse'@'localhost' IDENTIFIED BY 't3achTh3Childr3n';
 GRANT USAGE ON *.* TO 'warehouse'@'localhost';
