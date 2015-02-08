c334c62a40b39859a236c696dc2203c74792fd26
markroper <markroper@gmail.com>
2015-02-07 21:25:35 -0500

    Fixing threading issue surfaced by @jodamn & mentioned in StackOverflow

--- warehouse/src/main/java/com/scholarscore/api/security/config/CustomUsernamePasswordAuthenticationFilter.java
+++ warehouse/src/main/java/com/scholarscore/api/security/config/CustomUsernamePasswordAuthenticationFilter.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,62 +23,39 @@ import com.scholarscore.models.LoginRequest;
  *
  */
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    private static final String APPLICATION_JSON = "application/json";
-    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String HTTP_VERB = "POST";
     
-    private String jsonUsername;
-    private String jsonPassword;
+    private LoginRequest resolveLoginRequest(HttpServletRequest req) {
+        LoginRequest loginRequest = null;
+        try {
+            //HttpServletRequest can be read only once
+            StringBuffer sb = new StringBuffer();
+            String line = null;
 
-    @Override
-    protected String obtainPassword(HttpServletRequest request) {
-        String password = null; 
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null){
+                sb.append(line);
+            }
 
-        if (APPLICATION_JSON.equals(request.getHeader(CONTENT_TYPE))) {
-            password = this.jsonPassword;
-        }else{
-            password = super.obtainPassword(request);
+            //json transformation
+            ObjectMapper mapper = new ObjectMapper();
+            loginRequest = mapper.readValue(sb.toString(), LoginRequest.class);
+        } catch (Exception e) {
         }
-
-        return password;
+        return loginRequest;
     }
-
-    @Override
-    protected String obtainUsername(HttpServletRequest request){
-        String username = null;
-
-        if (APPLICATION_JSON.equals(request.getHeader(CONTENT_TYPE))) {
-            username = this.jsonUsername;
-        }else{
-            username = super.obtainUsername(request);
-        }
-
-        return username;
-    }
-
+    
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
-        if (APPLICATION_JSON.equals(request.getHeader(CONTENT_TYPE))) {
-            try {
-                //HttpServletRequest can be read only once
-                StringBuffer sb = new StringBuffer();
-                String line = null;
-
-                BufferedReader reader = request.getReader();
-                while ((line = reader.readLine()) != null){
-                    sb.append(line);
-                }
-
-                //json transformation
-                ObjectMapper mapper = new ObjectMapper();
-                LoginRequest loginRequest = mapper.readValue(sb.toString(), LoginRequest.class);
-
-                this.jsonUsername = loginRequest.getUsername();
-                this.jsonPassword = loginRequest.getPassword();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (!request.getMethod().equals(HTTP_VERB)) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
         }
 
-        return super.attemptAuthentication(request, response);
+        LoginRequest loginRequest = this.resolveLoginRequest(request);
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), loginRequest.getPassword());
+
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
     }
 }
