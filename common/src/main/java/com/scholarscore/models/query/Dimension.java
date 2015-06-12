7e71fd45529ee0c4b78154e696b09206f1822268
markroper <markroper@gmail.com>
2015-06-12 11:01:26 -0400

    Extends the Dimension and Measure enums to contain the information needed to validate queries and resolve actual field values available in a query.

--- common/src/main/java/com/scholarscore/models/query/Dimension.java
+++ common/src/main/java/com/scholarscore/models/query/Dimension.java
@@ -1,31 +1,47 @@
 package com.scholarscore.models.query;
 
+import com.scholarscore.models.Course;
+import com.scholarscore.models.GradeLevel;
+import com.scholarscore.models.School;
+import com.scholarscore.models.Section;
+import com.scholarscore.models.Student;
+import com.scholarscore.models.SubjectArea;
+import com.scholarscore.models.Teacher;
+
 /**
  * Enumerates the supported dimensions of the warehouse report and querying model.
  * Think of dimensions as those attributes you might GROUP BY in an aggregate SELECT
  * statement in SQL.
  * 
+ * Each enum value contains an array of the associated available classes
+ * that can be used to resolve the specific fields available for that enum.
+ * 
  * @author markroper
- *
  */
 public enum Dimension {
-    COURSE,
-    SECTION,
-    SUBJECT_AREA,
-    GRADE_LEVEL,
-    SCHOOL,
-    DISTRICT,
-    TEACHER,
+    COURSE(new Class[]{ Course.class, SubjectArea.class, GradeLevel.class, School.class }),
+    SECTION(new Class[]{ Section.class, Course.class, SubjectArea.class, GradeLevel.class, School.class }),
+    SUBJECT_AREA(new Class[]{ SubjectArea.class, School.class }),
+    GRADE_LEVEL(new Class[]{ GradeLevel.class, School.class }),
+    SCHOOL(new Class[]{ School.class }),
+    DISTRICT(new Class[]{}), //TODO: populate with SchoolDistrict class, when created
+    TEACHER(new Class[]{ Teacher.class }),
     //Student dimensions
-    STUDENT,
+    STUDENT(new Class[]{ Student.class }),
     //TODO: figure out how we want to model the sub-attributes for a student like:
-    GENDER,
-    FREE_LUNCH, //true if the student's family qualifies for free or reduce priced lunch, otherwise false
-    AGE,
-    GRADE_REPEATER, //true if the student is presently repeating a grade, otherwise false
-    ETHNICITY,
-    RACE,
-    ELL, //true if the student is designated an english language learner, otherwise false
-    SPECIAL_ED, //true if the student has an Individual Education Plan (IEP), otherwise false
-    CITY_OF_RESIDENCE;
+    GENDER(new Class[]{}),
+    FREE_LUNCH(new Class[]{}), //true if the student's family qualifies for free or reduce priced lunch, otherwise false
+    AGE(new Class[]{}),
+    GRADE_REPEATER(new Class[]{}), //true if the student is presently repeating a grade, otherwise false
+    ETHNICITY(new Class[]{}),
+    RACE(new Class[]{}),
+    ELL(new Class[]{}), //true if the student is designated an english language learner, otherwise false
+    SPECIAL_ED(new Class[]{}), //true if the student has an Individual Education Plan (IEP), otherwise false
+    CITY_OF_RESIDENCE(new Class[]{});
+    
+    private Class[] availableClasses;
+    
+    private Dimension(Class[] availableFields) {
+        this.availableClasses = availableFields;
+    }
 }
--- common/src/main/java/com/scholarscore/models/query/Measure.java
+++ common/src/main/java/com/scholarscore/models/query/Measure.java
@@ -1,24 +1,53 @@
 package com.scholarscore.models.query;
 
+import com.scholarscore.models.AttendanceAssignment;
+import com.scholarscore.models.GradedAssignment;
+
 /**
  * Enumerates the supported measures, or scalar values, supported within the 
  * reporting engine.  Think of these as the value aggregated in a SQL query, 
- * it should be reduceable to to a numeric value, the thing being counted.
+ * it should be reduced to to a numeric value, the thing being counted.
+ * 
+ * Each enum value contains an array of the associated available classes
+ * that can be used to resolve additional information about the Measure and its
+ * use.  The enum also contains an array of Dimensions that are incompatible with 
+ * that Measure.
+ * 
+ * Invalid Dimensions & Measure combinations:
+ * 1) GPA & ATTENDENCE Measures cannot be paired with TEACHER Dimension in a query 
+ * because there is no valid join between either of these and the teacher table.
+ * 2) TODO: identify any other invalid combinations?
  * 
  * @author markroper
  *
  */
 public enum Measure {
-    ATTENDANCE,
+    ATTENDANCE(new Class[]{ AttendanceAssignment.class }, new Dimension[]{ Dimension.TEACHER }),
     //Behavioral measures
-    DEMERITS,
-    MERITS,
-    DETENTIONS,
-    HOMEWORK_CLUBS,
-    SUSPENSIONS,
+    DEMERITS( new Class[]{}, new Dimension[]{} ),
+    MERITS( new Class[]{}, new Dimension[]{} ),
+    DETENTIONS( new Class[]{}, new Dimension[]{} ),
+    HOMEWORK_CLUBS( new Class[]{}, new Dimension[]{} ),
+    SUSPENSIONS( new Class[]{}, new Dimension[]{} ),
     //Academic measures
-    GPA,
-    COURSE_GRADE,
-    ASSIGNMENT_GRADE, //Could be test, HW, report, presentation, & so on
-    HW_COMPLETION;
+    GPA( new Class[]{}, new Dimension[]{ Dimension.TEACHER } ),
+    COURSE_GRADE( new Class[]{}, new Dimension[]{} ),
+    ASSIGNMENT_GRADE( new Class[]{ GradedAssignment.class }, new Dimension[]{} ), //Could be test, HW, report, presentation, & so on
+    HW_COMPLETION( new Class[]{}, new Dimension[]{} );
+    
+    private Class[] availableClasses;
+    private Dimension[] invalidDimensions;
+    
+    private Measure(Class[] availableFields, Dimension[] invalidDimensions) {
+        this.availableClasses = availableFields;
+        this.invalidDimensions = invalidDimensions;
+    }
+
+    public Class[] getAvailableFields() {
+        return availableClasses;
+    }
+
+    public Dimension[] getInvalidDimensions() {
+        return invalidDimensions;
+    }
 }
