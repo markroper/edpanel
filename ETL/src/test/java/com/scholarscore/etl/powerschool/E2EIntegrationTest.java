c64cc37a219b88dd355a51e2fbc5dbec1d005fe3
Matt Greenwood <matt@mjgreenwood.net>
2015-07-04 05:47:42 -0400

    small refactoring, removed EntityIdResponse updated test to only include etl.xml

--- ETL/src/test/java/com/scholarscore/etl/powerschool/E2EIntegrationTest.java
+++ ETL/src/test/java/com/scholarscore/etl/powerschool/E2EIntegrationTest.java
@@ -13,7 +13,7 @@ import static org.testng.Assert.assertNotNull;
  * Created by mattg on 7/3/15.
  */
 @Test
-@ContextConfiguration(locations = {"classpath:etl.xml", "classpath*:powerschool.xml", "classpath*:scholarscore.xml"})
+@ContextConfiguration(locations = {"classpath:etl.xml"})
 public class E2EIntegrationTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
