a3bb2e6de1926d695f696383a6b469571148dcc1
Jordan Winch <jordan_winch@intuit.com>
2015-07-23 00:09:56 -0400

    first step of deanslist ETL work

---
+++ DL_ETL/src/main/java/com/scholarscore/etl/deanslist/api/response/BehaviorResponse.java
@@ -0,0 +1,7 @@
+package com.scholarscore.etl.deanslist.api.response;
+
+/**
+ * Created by jwinch on 7/23/15.
+ */
+public class BehaviorResponse {
+}
---
+++ DL_ETL/src/test/java/com/scholarscore/etl/deanslist/DeansListFunctionalTest.java
@@ -0,0 +1,38 @@
+package com.scholarscore.etl.deanslist;
+
+import com.scholarscore.etl.deanslist.client.IDeansListClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+/**
+ * A simple test to invoke the API and get data
+ * 
+ * Created by jwinch on 7/22/15.
+ */
+@Test
+@ContextConfiguration(locations = { "classpath:deanslist.xml" })
+public class DeansListFunctionalTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private IDeansListClient client;
+    
+    public void testGetStudents() { 
+        client.getStudents();
+    }
+    
+    public void testGetBehaviorData() { 
+        client.getBehaviorData();
+    }
+    
+    /* 
+    * 
+    * public void testLoadSchools() {
+        SchoolsResponse response = client.getSchools();
+        assertNotNull(response);
+        assertNotNull(response.schools);
+        assertTrue(response.schools.school.size() > 0);
+    }*/
+    
+}
