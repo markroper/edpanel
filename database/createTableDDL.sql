52cdc179f48d6a919063a999a5655a4d4705a5a2
markroper <markroper@gmail.com>
2015-09-14 14:55:41 -0400

    Adds teacher association with sections to the arbitrary data generator.

--- database/createTableDDL.sql
+++ database/createTableDDL.sql
@@ -61,24 +61,6 @@ CREATE TABLE `scholar_warehouse`.`teacher` (
     ON DELETE SET NULL
     ON UPDATE CASCADE)
   ENGINE = InnoDB;
-  
-CREATE TABLE `scholar_warehouse`.`teacher_section` (
-  `teacher_section_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The auto incrementing primary key identity column',
-  `teacher_fk` BIGINT UNSIGNED COMMENT 'The FK to the teacher table',
-  `section_fk` BIGINT UNSIGNED COMMENT 'The FK to the section table',
-  `role` VARCHAR(256) NULL COMMENT 'Indicates the role the teacher has in the section',
-  PRIMARY KEY (`teacher_section_id`),
-  CONSTRAINT `teacher_section_teacher_fk`
-    FOREIGN KEY(`teacher_fk`)
-    REFERENCES `scholar_warehouse`.`teacher`(`teacher_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `teacher_section_section_fk`
-    FOREIGN KEY(`section_fk`)
-    REFERENCES `scholar_warehouse`.`section`(`section_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-  ENGINE = InnoDB;
 
 CREATE TABLE `scholar_warehouse`.`administrator` (
   `administrator_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The auto incrementing primary key identity column',
@@ -158,6 +140,24 @@ CREATE TABLE `scholar_warehouse`.`section` (
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
+
+CREATE TABLE `scholar_warehouse`.`teacher_section` (
+  `teacher_section_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The auto incrementing primary key identity column',
+  `teacher_fk` BIGINT UNSIGNED COMMENT 'The FK to the teacher table',
+  `section_fk` BIGINT UNSIGNED COMMENT 'The FK to the section table',
+  `role` VARCHAR(256) NULL COMMENT 'Indicates the role the teacher has in the section',
+  PRIMARY KEY (`teacher_section_id`),
+  CONSTRAINT `teacher_section_teacher_fk`
+    FOREIGN KEY(`teacher_fk`)
+    REFERENCES `scholar_warehouse`.`teacher`(`teacher_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `teacher_section_section_fk`
+    FOREIGN KEY(`section_fk`)
+    REFERENCES `scholar_warehouse`.`section`(`section_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+  ENGINE = InnoDB;
 
 CREATE TABLE `scholar_warehouse`.`assignment` (
   `assignment_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The auto incrementing primary key identity column',
