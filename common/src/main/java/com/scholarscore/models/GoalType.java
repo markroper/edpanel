020b268a90faf238c1c31e76892fe758e1f1cb84
cwallac <christopher.wallace@students.olin.edu>
2015-09-22 21:47:00 -0400

    Added short java doc

--- common/src/main/java/com/scholarscore/models/GoalType.java
+++ common/src/main/java/com/scholarscore/models/GoalType.java
@@ -3,6 +3,8 @@ package com.scholarscore.models;
 import java.io.Serializable;
 
 /**
+ * Enum for different types of goals.
+ * ATTENDANCE, TERM AND SECTION goals are yet to be implemented
  * Created by cwallace on 9/17/2015.
  */
 public enum GoalType implements Serializable {
