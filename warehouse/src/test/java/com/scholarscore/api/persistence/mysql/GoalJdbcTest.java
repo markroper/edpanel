f6fb9e7728ea4b67f1495ab1386190a72671a1dc
cwallac <christopher.wallace@students.olin.edu>
2015-09-23 22:32:37 -0400

    Added post check for goals

--- warehouse/src/test/java/com/scholarscore/api/persistence/mysql/GoalJdbcTest.java
+++ warehouse/src/test/java/com/scholarscore/api/persistence/mysql/GoalJdbcTest.java
@@ -1,9 +1,6 @@
 package com.scholarscore.api.persistence.mysql;
 
-import com.scholarscore.models.BehaviorGoal;
-import com.scholarscore.models.Course;
-import com.scholarscore.models.Section;
-import com.scholarscore.models.Term;
+import com.scholarscore.models.*;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertNotNull;
@@ -14,8 +11,19 @@ import static org.testng.Assert.assertNull;
  */
 @Test(groups = { "functional"})
 public class GoalJdbcTest extends BaseJdbcTest {
-    public void testGoalCrud() {
+    public void testBehaviorGoalCrud() {
         BehaviorGoal goalToCreate = new BehaviorGoal(behaviorGoal);
+        goalToCreate.setStudent(createStudent());
+        goalToCreate.setTeacher(createTeacher());
+        Long goalId = goalDao.createGoal(goalToCreate.getStudent().getId(), goalToCreate);
+        assertNotNull(goalId, "Expected non-null sectionId from create of section");
+
+        goalDao.delete(goalToCreate.getStudent().getId(), goalId);
+        assertNull(goalDao.select(goalToCreate.getStudent().getId(), goalId), "Expected section to be null after delete operation");
+    }
+
+    public void testAssignmentGoalCrud() {
+        AssignmentGoal goalToCreate = new AssignmentGoal(assignmentGoal);
         goalToCreate.setStudent(createStudent());
         goalToCreate.setTeacher(createTeacher());
         Long goalId = goalDao.createGoal(goalToCreate.getStudent().getId(), goalToCreate);
