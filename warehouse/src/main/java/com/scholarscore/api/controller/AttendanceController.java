60c49eb60aa45df88363f198c7bdf3596f653422
markroper <markroper@gmail.com>
2015-09-28 18:49:02 -0400

    Adds school day and attendance controllers.

---
+++ warehouse/src/main/java/com/scholarscore/api/controller/AttendanceController.java
@@ -0,0 +1,115 @@
+package com.scholarscore.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.scholarscore.api.ApiConsts;
+import com.scholarscore.models.EntityId;
+import com.scholarscore.models.attendance.Attendance;
+import com.scholarscore.models.attendance.DayOfSchool;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+@Controller
+@RequestMapping(ApiConsts.API_V1_ENDPOINT + "/schools/{schoolId}/students/{studentId}/attendance")
+public class AttendanceController extends BaseController {
+    @ApiOperation(
+            value = "Get all student attendance entries", 
+            notes = "Retrieve all student attendance entries within a school", 
+            response = List.class)
+    @RequestMapping(
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getAll(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "studentId", required = true, value = "Student ID")
+            @PathVariable(value="studentId") Long studentId) {
+        return respond(pm.getAttendanceManager().getAllStudentAttendance(schoolId, studentId));
+    }
+    
+    @ApiOperation(
+            value = "Get all student attendance entries in a term", 
+            notes = "Retrieve all student attendance entries within a term", 
+            response = List.class)
+    @RequestMapping(
+            value = "/years/{schoolYearId}/terms/{termId}",
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getAll(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "studentId", required = true, value = "Student ID")
+            @PathVariable(value="studentId") Long studentId,
+            @ApiParam(name = "schoolYearId", required = true, value = "School Year ID")
+            @PathVariable(value="schoolYearId") Long schoolYearId,
+            @ApiParam(name = "termId", required = true, value = "Term ID")
+            @PathVariable(value="termId") Long termId) {
+        return respond(pm.getAttendanceManager().getAllStudentAttendanceInTerm(schoolId, studentId, schoolYearId, termId));
+    }
+    
+    @ApiOperation(
+            value = "Get a student attendance object by ID", 
+            notes = "Retrieve a student attendance object its ID", 
+            response = Attendance.class)
+    @RequestMapping(
+            value = "/{attendanceId}", 
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getASchoolDay(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "studentId", required = true, value = "Student ID")
+            @PathVariable(value="studentId") Long studentId,
+            @ApiParam(name = "attendanceId", required = true, value = "Attendance ID")
+            @PathVariable(value="attendanceId") Long attendanceId) {
+        return respond(pm.getAttendanceManager().getStudentAttendance(schoolId, studentId, attendanceId));
+    }
+    
+    @ApiOperation(
+            value = "Create an attendance entry for a student", 
+            notes = "Creates, assigns an ID, persists and returns a an attendance ID",
+            response = EntityId.class)
+    @RequestMapping(
+            method = RequestMethod.POST, 
+            produces = {JSON_ACCEPT_HEADER})
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity createAttendance(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "studentId", required = true, value = "Student ID")
+            @PathVariable(value="studentId") Long studentId,
+            @RequestBody @Valid Attendance attendance) {
+        return respond(pm.getAttendanceManager().createAttendanceObject(schoolId, studentId, attendance));
+    }
+    
+    @ApiOperation(
+            value = "Delete an attendance entry", 
+            response = Void.class)
+    @RequestMapping(
+            value = "/{attendanceId}", 
+            method = RequestMethod.DELETE, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity deleteAttendance(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "studentId", required = true, value = "Student ID")
+            @PathVariable(value="studentId") Long studentId,
+            @ApiParam(name = "attendanceId", required = true, value = "Attendance ID")
+            @PathVariable(value="attendanceId") Long attendanceId) {
+        return respond(pm.getAttendanceManager().deleteAttendance(schoolId, studentId, attendanceId));
+    }
+}
---
+++ warehouse/src/main/java/com/scholarscore/api/controller/SchoolDayController.java
@@ -0,0 +1,103 @@
+package com.scholarscore.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.scholarscore.api.ApiConsts;
+import com.scholarscore.models.EntityId;
+import com.scholarscore.models.SchoolYear;
+import com.scholarscore.models.attendance.DayOfSchool;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+@Controller
+@RequestMapping(ApiConsts.API_V1_ENDPOINT + "/schools/{schoolId}/days")
+public class SchoolDayController extends BaseController {
+    @ApiOperation(
+            value = "Get all school days", 
+            notes = "Retrieve all school days within a school", 
+            response = List.class)
+    @RequestMapping(
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getAll(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId) {
+        return respond(pm.getSchoolDayManager().getAllDays(schoolId));
+    }
+    
+    @ApiOperation(
+            value = "Get all school days in a school year", 
+            notes = "Retrieve all school days within a school year", 
+            response = List.class)
+    @RequestMapping(
+            value = "/years/{schoolYearId}", 
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getAllDaysInYear(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "schoolYearId", required = true, value = "School year ID")
+            @PathVariable(value="schoolYearId") Long schoolYearId) {
+        return respond(pm.getSchoolDayManager().getAllDaysInYear(schoolId, schoolYearId));
+    }
+    
+    @ApiOperation(
+            value = "Get a school day by ID", 
+            notes = "Retrieve a school day by its ID", 
+            response = DayOfSchool.class)
+    @RequestMapping(
+            value = "/{schoolDayId}", 
+            method = RequestMethod.GET, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity getASchoolDay(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "schoolDayId", required = true, value = "School day ID")
+            @PathVariable(value="schoolDayId") Long schoolDayId) {
+        return respond(pm.getSchoolDayManager().getDay(schoolId, schoolDayId));
+    }
+    
+    @ApiOperation(
+            value = "Create a school day", 
+            notes = "Creates, assigns an ID, persists and returns a school day ID",
+            response = EntityId.class)
+    @RequestMapping(
+            method = RequestMethod.POST, 
+            produces = {JSON_ACCEPT_HEADER})
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity createSchoolDay(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @RequestBody @Valid DayOfSchool schoolDay) {
+        return respond(pm.getSchoolDayManager().createSchoolDay(schoolId, schoolDay));
+    }
+    
+    @ApiOperation(
+            value = "Delete a school day", 
+            response = Void.class)
+    @RequestMapping(
+            value = "/{schoolDayId}", 
+            method = RequestMethod.DELETE, 
+            produces = { JSON_ACCEPT_HEADER })
+    @SuppressWarnings("rawtypes")
+    public @ResponseBody ResponseEntity deleteSchoolYear(
+            @ApiParam(name = "schoolId", required = true, value = "School ID")
+            @PathVariable(value="schoolId") Long schoolId,
+            @ApiParam(name = "schoolDayId", required = true, value = "School day ID")
+            @PathVariable(value="schoolDayId") Long schoolDayId) {
+        return respond(pm.getSchoolDayManager().deleteSchoolYear(schoolId, schoolDayId));
+    }
+}
