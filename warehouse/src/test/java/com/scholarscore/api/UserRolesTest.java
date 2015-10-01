3fd30c90ca653ee1fc6522ea763be8183561ad6b
jordan <jodamn AT gmail>
2015-10-01 13:47:44 -0400

    [WIP] checking in so I can switch back to development and fix the build I broke

---
+++ warehouse/src/test/java/com/scholarscore/api/UserRolesTest.java
@@ -0,0 +1,17 @@
+package com.scholarscore.api;
+
+import com.scholarscore.api.controller.base.IntegrationBase;
+import org.testng.annotations.Test;
+
+/**
+ * User: jordan
+ * Date: 10/1/15
+ * Time: 1:37 PM
+ */
+public class UserRolesTest extends IntegrationBase {
+    
+    @Test
+    public void testUserRoles() { 
+        
+    }
+}
