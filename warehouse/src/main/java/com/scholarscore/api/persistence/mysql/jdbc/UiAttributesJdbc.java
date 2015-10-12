39b5cd2a6c7e82d388dbe41a7d840978f97cf19f
markroper <markroper@gmail.com>
2015-10-12 15:00:13 -0400

    Adds UI attributes to the API & persistence layers.

---
+++ warehouse/src/main/java/com/scholarscore/api/persistence/mysql/jdbc/UiAttributesJdbc.java
@@ -0,0 +1,44 @@
+package com.scholarscore.api.persistence.mysql.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+
+import com.scholarscore.api.persistence.UiAttributePersistence;
+import com.scholarscore.models.UiAttributes;
+
+public class UiAttributesJdbc implements UiAttributePersistence {
+    @Autowired
+    private HibernateTemplate hibernateTemplate;
+
+    public UiAttributesJdbc() {
+    }
+    
+    public void setHibernateTemplate(HibernateTemplate template) {
+        this.hibernateTemplate = template;
+    }
+    
+    @Override
+    public UiAttributes select(long schoolId) {
+        String query = "from ui_attributes u join u.school s where s.id = :schoolId";
+        @SuppressWarnings("unchecked")
+        List<UiAttributes> uiAttributes = 
+            (List<UiAttributes>) hibernateTemplate.findByNamedParam(query, "schoolId", new Long(schoolId));
+        if (uiAttributes.size() == 1) {
+            return uiAttributes.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public void createUiAttributes(long schoolId, UiAttributes attrs) {
+        hibernateTemplate.merge(attrs);    
+    }
+
+    @Override
+    public void replaceUiAttributes(long schoolId, UiAttributes attrs) {
+        hibernateTemplate.merge(attrs);    
+    }
+
+}
