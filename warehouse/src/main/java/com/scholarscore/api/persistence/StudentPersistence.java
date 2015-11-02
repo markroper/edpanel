b1159c550e6d04b5b36d6cfe35ccdd7e099321d3
jordan <jodamn@gmail.com>
2015-11-02 01:38:10 -0500

    more cleanup

--- warehouse/src/main/java/com/scholarscore/api/persistence/StudentPersistence.java
+++ warehouse/src/main/java/com/scholarscore/api/persistence/StudentPersistence.java
@@ -1,11 +1,8 @@
 package com.scholarscore.api.persistence;
 
-import com.scholarscore.models.PrepScore;
 import com.scholarscore.models.user.Student;
 
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 
 public interface StudentPersistence {
     Collection<Student> selectAll(Long schoolId);
