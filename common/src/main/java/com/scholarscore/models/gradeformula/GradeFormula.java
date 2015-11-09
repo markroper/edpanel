8aa5a9e7ea3989c9f530247c18c993c3f0b0bcdb
markroper <markroper@gmail.com>
2015-11-09 15:59:51 -0500

    Adds doc to GradeFormula and cleans up messes.

--- common/src/main/java/com/scholarscore/models/gradeformula/GradeFormula.java
+++ common/src/main/java/com/scholarscore/models/gradeformula/GradeFormula.java
@@ -15,15 +15,40 @@ import java.util.Objects;
 import java.util.Set;
 
 /**
+ * PowerSchool breaks the logical concept of a Grade Formula into three objects, each of which
+ * has a one to many relationship to the next. GradeSetup < GradeFormula < GradeFormulaWeighting.
+ * All three of these concepts are expressed in this class.
+ *
+ * In PowerSchool, GradeSetup can be a hierarchical graph, as in the following example:
+ *
+ *                          school year
+ *                         /           \
+ *                    semester 1    semester 2
+ *                   /         \    /         \
+ *                  Q1         Q2  Q3         Q4
+ *
+ * Each of these grade setups may have its own GradeFormula, which in turn have their own weights.  A setup
+ * does not need to have a formula, and in these cases a strait sum(awarded_points)/sum(available_points) formula
+ * is used to resolve a grade for a section over a reporting period.
+ *
+ * In the case where a grade formula is supplied, weightings can be specified by either assignment type,
+ * asignment ID, or child grade formula scores.
+ *
  * Created by markroper on 11/9/15.
  */
 @SuppressWarnings("serial")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GradeFormula implements Serializable {
+    //Will be set to the source system grade formula reporting term ID
     Long id;
+    //Set to the grade setup name
     String name;
+    String sourceSystemDescription;
+    //Start date of the reporting term
     Date startDate;
+    //End date of the reporting term
     Date endDate;
+    //Parent grade formula (e.g. a Quarter's grade formula may have a parent semester grade formula)
     Long parentId;
     Set<GradeFormula> children = new HashSet<>();
     //TODO: missing low score to discard?
@@ -38,6 +63,7 @@ public class GradeFormula implements Serializable {
     public GradeFormula(Map<AssignmentType, Double> weight) {
         this.assignmentTypeWeights = weight;
     }
+
     public Double calculateGrade(Set<StudentAssignment> studentAssignments) {
         Double numerator = 0D;
         Double denominator = 0D;
@@ -182,6 +208,14 @@ public class GradeFormula implements Serializable {
         this.assignmentWeights = assignmentWeights;
     }
 
+    public String getSourceSystemDescription() {
+        return sourceSystemDescription;
+    }
+
+    public void setSourceSystemDescription(String sourceSystemDescription) {
+        this.sourceSystemDescription = sourceSystemDescription;
+    }
+
     public Map<Long, Double> getChildWeights() {
         return childWeights;
     }
@@ -193,7 +227,7 @@ public class GradeFormula implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, startDate, endDate, parentId, children, assignmentTypeWeights,
-                assignmentWeights, childWeights);
+                assignmentWeights, childWeights, sourceSystemDescription);
     }
 
     @Override
@@ -211,6 +245,7 @@ public class GradeFormula implements Serializable {
                 && Objects.equals(this.endDate, other.endDate)
                 && Objects.equals(this.parentId, other.parentId)
                 && Objects.equals(this.children, other.children)
+                && Objects.equals(this.sourceSystemDescription, other.sourceSystemDescription)
                 && Objects.equals(this.assignmentTypeWeights, other.assignmentTypeWeights)
                 && Objects.equals(this.assignmentWeights, other.assignmentWeights)
                 && Objects.equals(this.childWeights, other.childWeights);
