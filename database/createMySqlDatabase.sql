281ae0b8f7e394cfdeb2faf64f3c824c06d64818
jordan <jodamn@gmail.com>
2015-11-10 17:40:46 -0500

    just need this branch temporarily to easily deploy to an prod-like ubuntu VM for testing

--- database/createMySqlDatabase.sql
+++ database/createMySqlDatabase.sql
@@ -1,5 +1,7 @@
 -- currently not used automatically, run manually the first time to provision the user
 -- note: the following commands work the same regardless of if the scholar_warehouse database exists
 CREATE USER 'warehouse'@'localhost' IDENTIFIED BY 't3achTh3Childr3n';
+-- TODO in PROD: this line should be: 
+-- GRANT USAGE ON *.* TO 'warehouse'@'localhost REQUIRE SSL';
 GRANT USAGE ON *.* TO 'warehouse'@'localhost';
 GRANT ALL PRIVILEGES ON scholar_warehouse.* TO 'warehouse'@'localhost';
