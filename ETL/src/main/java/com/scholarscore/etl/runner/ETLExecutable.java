2c619909bbb59dc0710e16ebadc64094c4cc69f4
jordan <jodamn@gmail.com>
2015-11-12 22:02:12 -0500

    re-enabling a commented out config line

--- ETL/src/main/java/com/scholarscore/etl/runner/ETLExecutable.java
+++ ETL/src/main/java/com/scholarscore/etl/runner/ETLExecutable.java
@@ -8,7 +8,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Date: 11/12/15
  * Time: 5:21 PM
  */
-public class ETLExecutable {
+public class EtlExecutable {
+    
+    private static final String ETL_SPRING_CONTEXT_FILENAME = "etl.xml";
     
     public static void main(String... args) {
         loadSpringConfigAndLaunchETL();
@@ -16,8 +18,8 @@ public class ETLExecutable {
 
     public static void loadSpringConfigAndLaunchETL() {
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("etl.xml");
-        ETLRunner runner = context.getBean(ETLRunner.class);
+                new ClassPathXmlApplicationContext(ETL_SPRING_CONTEXT_FILENAME);
+        EtlRunner runner = context.getBean(EtlRunner.class);
         runner.migrateDistrict();
     }
     
