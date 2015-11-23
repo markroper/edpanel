298e4010a2c75e81d7595ed4fe93b1f32e19bf21
jordan <jodamn@gmail.com>
2015-11-23 13:41:01 -0500

    work in progress hiding private fields from json requests/responses

--- warehouse/src/test/java/com/scholarscore/api/controller/AuthenticationIntegrationTest.java
+++ warehouse/src/test/java/com/scholarscore/api/controller/AuthenticationIntegrationTest.java
@@ -18,7 +18,7 @@ import static org.testng.Assert.assertNotNull;
 @Test(groups = {"integration"})
 public class AuthenticationIntegrationTest extends IntegrationBase {
 
-    // break this out into resource file (or something)0
+    // break this out into resource file (or something)
     private static final String USER_PERMISSIONS_LOGIN = "student_user";
     private static final String USER_PERMISSIONS_PASS = "student_user";
     
@@ -39,7 +39,6 @@ public class AuthenticationIntegrationTest extends IntegrationBase {
     public void testStudentLogin() {
         Student user = new Student();
         user.setPassword("password");
-        user.setEnabled(true);
         user.setUsername(UUID.randomUUID().toString());
         User studentUser = userValidatingExecutor.create(user, "Creating test student user");
         assertNotNull(studentUser);
