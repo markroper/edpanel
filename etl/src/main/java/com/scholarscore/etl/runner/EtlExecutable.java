6d4bce175cbe03483220c7c006823f7ed09f5a70
Matt Greenwood <matt@mjgreenwood.net>
2015-11-24 11:27:56 -0500

    initial creation of EtlRunner changes to support command line arguments using commons-cli for obtaining the gpa file

--- etl/src/main/java/com/scholarscore/etl/runner/EtlExecutable.java
+++ etl/src/main/java/com/scholarscore/etl/runner/EtlExecutable.java
@@ -1,7 +1,10 @@
 package com.scholarscore.etl.runner;
 
+import org.apache.commons.cli.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
 
 /**
  * User: jordan
@@ -9,18 +12,41 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Time: 5:21 PM
  */
 public class EtlExecutable {
-    
+
+    public static final String ARGS_GPA_FILE_SETTINGS = "g";
+    public static final String ARGS_GPA_FILE_SETTINGS_LONG = "gpa-file";
+
     private static final String ETL_SPRING_CONTEXT_FILENAME = "etl.xml";
     
-    public static void main(String... args) {
-        loadSpringConfigAndLaunchETL();
+    public static void main(String... args) throws ParseException {
+
+        EtlSettings settings = parseSettings(args);
+        loadSpringConfigAndLaunchETL(settings);
     }
 
-    public static void loadSpringConfigAndLaunchETL() {
+    private static EtlSettings parseSettings(String[] args) throws ParseException {
+
+        Options options = new Options();
+        //String opt, String longOpt, boolean hasArg, String description)
+        options.addOption(new Option(ARGS_GPA_FILE_SETTINGS, ARGS_GPA_FILE_SETTINGS_LONG, false,
+                "Specify the GPA file to parse as part of ETL loading from the external source"));
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse( options, args);
+
+        EtlSettings settings = new EtlSettings();
+
+        if (cmd.hasOption(ARGS_GPA_FILE_SETTINGS)) {
+            settings.setGpaImportFile(new File(cmd.getOptionValue(ARGS_GPA_FILE_SETTINGS)));
+        }
+        return settings;
+    }
+
+    public static void loadSpringConfigAndLaunchETL(EtlSettings settings) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext(ETL_SPRING_CONTEXT_FILENAME);
         EtlRunner runner = context.getBean(EtlRunner.class);
-        runner.migrateDistrict();
+        runner.migrateDistrict(settings);
     }
     
 }
