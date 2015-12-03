f913732b1630b37a1025e2dbc06b2d16cb153f47
markroper <markroper@gmail.com>
2015-12-03 11:48:34 -0500

    derp

--- database/createTableDDL.sql
+++ database/createTableDDL.sql
@@ -454,6 +454,8 @@ insert into `scholar_warehouse`.`authorities` (user_id, authority) values (2, 'S
 insert into `scholar_warehouse`.`authorities` (user_id, authority) values (3, 'STUDENT');
   
 insert into `scholar_warehouse`.`school` (school_name) values ('FirstSchool');
+
+insert into `scholar_warehouse`.`administrator` (administrator_name, administrator_user_fk, school_fk) values ('Matt Greenwood', 1, null);
 insert into `scholar_warehouse`.`student`       (student_name, student_user_fk, school_fk)             values ('StudentUser', 2, 1);
 insert into `scholar_warehouse`.`student`       (student_name, student_user_fk, school_fk)             values ('StudentUser2', 3, 1);
 
