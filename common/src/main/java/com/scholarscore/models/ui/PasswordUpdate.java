090bc5ddc6ed6ba5f50d08123eb264de67f7b3af
jordan <jodamn@gmail.com>
2015-12-03 11:39:12 -0500

    bcrypting passwords set with the updatePassword endpoint, and take the password in a JSON object instead of as a parameter on the url

---
+++ common/src/main/java/com/scholarscore/models/ui/PasswordUpdate.java
@@ -0,0 +1,19 @@
+package com.scholarscore.models.ui;
+
+/**
+ * User: jordan
+ * Date: 12/3/15
+ * Time: 11:00 AM
+ */
+public class PasswordUpdate {
+    
+    private String newPassword;
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
