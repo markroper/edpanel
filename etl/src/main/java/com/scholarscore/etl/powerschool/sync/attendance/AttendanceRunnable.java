5a2edeaa96c349293a65bd191f87a48b5ecaaf62
Chris Wallace <christopher.wallace@students.olin.edu>
2016-01-04 16:37:33 -0500

    Fixed logic bug in ETL code"

--- etl/src/main/java/com/scholarscore/etl/powerschool/sync/attendance/AttendanceRunnable.java
+++ etl/src/main/java/com/scholarscore/etl/powerschool/sync/attendance/AttendanceRunnable.java
@@ -275,8 +275,11 @@ public class AttendanceRunnable implements Runnable, ISync<Attendance> {
                             if (null != expression.get(letter)) {
                                 for (Long sectionPeriod : expression.get(letter)) {
                                     if (sectionPeriod.equals(periodNumber) &&
-                                            schoolDay.getDate().isAfter(section.getStartDate())
-                                            && schoolDay.getDate().isBefore(section.getEndDate())) {
+                                            (schoolDay.getDate().isAfter(section.getStartDate())
+                                                || schoolDay.getDate().equals(section.getStartDate()))
+                                            && (schoolDay.getDate().isBefore(section.getEndDate())
+                                                || schoolDay.getDate().equals(section.getEndDate()))
+                                            ){
                                         a.setSection(section);
                                     }
                                 }
