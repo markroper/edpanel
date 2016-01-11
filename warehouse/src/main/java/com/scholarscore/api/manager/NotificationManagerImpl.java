388d053151d00a6833be4951af5f24fc0620490a
markroper <markroper@gmail.com>
2016-01-11 06:51:38 -0500

    Adds the manager implementations except for evaluateNotifications(), which I'm saving for last and which is most involved.

--- warehouse/src/main/java/com/scholarscore/api/manager/NotificationManagerImpl.java
+++ warehouse/src/main/java/com/scholarscore/api/manager/NotificationManagerImpl.java
@@ -2,6 +2,9 @@ package com.scholarscore.api.manager;
 
 import com.scholarscore.api.persistence.NotificationPersistence;
 import com.scholarscore.api.util.ServiceResponse;
+import com.scholarscore.api.util.StatusCode;
+import com.scholarscore.api.util.StatusCodeType;
+import com.scholarscore.api.util.StatusCodes;
 import com.scholarscore.models.EntityId;
 import com.scholarscore.models.notification.Notification;
 import com.scholarscore.models.notification.TriggeredNotification;
@@ -19,7 +22,8 @@ public class NotificationManagerImpl implements NotificationManager {
     @Autowired
     private OrchestrationManager pm;
 
-    private static final String NOTIFICATION = "Notification";
+    private static final String NOTIFICATION = "notification";
+    private static final String TRIGGERED_NOTIFICATION = "triggered notification";
 
     public void setNotificationPersistence(NotificationPersistence notificationPersistence) {
         this.notificationPersistence = notificationPersistence;
@@ -31,46 +35,76 @@ public class NotificationManagerImpl implements NotificationManager {
 
     @Override
     public ServiceResponse<Notification> getNotification(Long notificationId) {
-        return null;
+        Notification n = notificationPersistence.select(notificationId);
+        if(null == n) {
+            return new ServiceResponse<>(
+                    StatusCodes.getStatusCode(StatusCodeType.MODEL_NOT_FOUND, new Object[]{ NOTIFICATION, notificationId}));
+        };
+        return new ServiceResponse<>(n);
     }
 
     @Override
     public ServiceResponse<List<Notification>> getAllNotificationsForUser(Long userId) {
-        return null;
+        StatusCode code = pm.getUserManager().userExists(userId);
+        if(!code.isOK()) {
+            return new ServiceResponse<>(code);
+        }
+        return new ServiceResponse<>(notificationPersistence.selectAllForUser(userId));
     }
 
     @Override
     public ServiceResponse<List<Notification>> getAllNotifications() {
-        return null;
+        return new ServiceResponse<>(notificationPersistence.selectAll());
     }
 
     @Override
     public ServiceResponse<Void> evaluateNotifications(Long schoolId) {
+        //TODO: implement the notification evaluation
         return null;
     }
 
     @Override
     public ServiceResponse<EntityId> createNotification(Notification notification) {
-        return null;
+        return new ServiceResponse(notificationPersistence.insertNotification(notification));
     }
 
     @Override
     public ServiceResponse<Void> replaceNotification(Long notificationId, Notification notification) {
-        return null;
+        notificationPersistence.replaceNotification(notificationId, notification);
+        return new ServiceResponse<>((Void) null);
     }
 
     @Override
     public ServiceResponse<Void> deleteNotification(Long notificationId) {
-        return null;
+        notificationPersistence.deleteNotification(notificationId);
+        return new ServiceResponse<>((Void) null);
     }
 
     @Override
-    public ServiceResponse<List<TriggeredNotification>> getAllTriggeredNotificationsForUser(Long userId, Boolean includeInactive) {
-        return null;
+    public ServiceResponse<List<TriggeredNotification>>
+            getAllTriggeredNotificationsForUser(Long userId, Boolean includeInactive) {
+        StatusCode code = pm.getUserManager().userExists(userId);
+        if(!code.isOK()) {
+            return new ServiceResponse<>(code);
+        }
+        return new ServiceResponse<>(notificationPersistence.selectTriggeredForUser(userId ,includeInactive));
     }
 
     @Override
     public ServiceResponse<Void> dismissTriggeredNotification(Long notificationId, Long triggeredId, Long userId) {
-        return null;
+        StatusCode code = pm.getUserManager().userExists(userId);
+        if(!code.isOK()) {
+            return new ServiceResponse<>(code);
+        }
+        TriggeredNotification tn = notificationPersistence.selectTriggered(triggeredId);
+        if(null == tn) {
+            code = StatusCodes.getStatusCode(
+                            StatusCodeType.MODEL_NOT_FOUND,
+                            new Object[]{ TRIGGERED_NOTIFICATION, triggeredId});
+            return new ServiceResponse<>(code);
+        }
+        tn.setIsActive(false);
+        notificationPersistence.updateTriggeredNotification(notificationId, tn);
+        return new ServiceResponse<>((Void) null);
     }
 }
