8aa938556bbf0200c7e26b6516f9afb7c52a5ea7
jordan <jodamn@gmail.com>
2016-01-23 00:45:25 -0500

    fixing more issues

--- common/src/main/java/com/scholarscore/models/user/Person.java
+++ common/src/main/java/com/scholarscore/models/user/Person.java
@@ -2,6 +2,8 @@ package com.scholarscore.models.user;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.Objects;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @SuppressWarnings("serial")
 public abstract class Person extends User {
@@ -90,29 +92,26 @@ public abstract class Person extends User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Person staff = (Person) o;
-
-        if (homePhone != null ? !homePhone.equals(staff.homePhone) : staff.homePhone != null) { return false; }
-        if (sourceSystemUserId != null ? !sourceSystemUserId.equals(staff.sourceSystemUserId) : staff.sourceSystemUserId != null) { return false; }
-        if (sourceSystemId != null ? !sourceSystemId.equals(staff.sourceSystemId) : staff.sourceSystemId != null) {
-            return false;
-        }
-
-        return true;
+    public int hashCode() {
+        return 31 * super.hashCode() + Objects.hash(sourceSystemId, sourceSystemUserId, homePhone, currentSchoolId);
     }
 
     @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (homePhone != null ? homePhone.hashCode() : 0);
-        result = 31 * result + (sourceSystemId != null ? sourceSystemId.hashCode() : 0);
-        result = 31 * result + (sourceSystemUserId != null ? sourceSystemUserId.hashCode() : 0);
-        return result;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        return Objects.equals(this.sourceSystemId, other.sourceSystemId)
+                && Objects.equals(this.sourceSystemUserId, other.sourceSystemUserId)
+                && Objects.equals(this.homePhone, other.homePhone)
+                && Objects.equals(this.currentSchoolId, other.currentSchoolId);
     }
 
     @Override
