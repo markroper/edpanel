4a7c3ca0b0e3086667194cf5d65ac545580b9384
jordan <jodamn@gmail.com>
2016-01-26 12:00:18 -0500

    adding new gradle task to automatically refill DB from newly-attached dummy database file. adding copied shell script that actually handles the clearing of the existing database, the dumping of the dummy file into the db, and a sanity check that the number of students in the dataset is 500 or above

---
+++ database/populate_db.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DUMMY_DB_FILENAME=dev_scholar_warehouse.sql
+DATABASE_NAME=scholar_warehouse
+
+if [ ! -f "./$DUMMY_DB_FILENAME" ]; then
+    printf "Dummy DB population FAILED - Dummy DB file does not exist at $SCRIPT_DIR/$DUMMY_DB_FILENAME..."
+    exit 1
+fi
+
+##
+if [ $5 ]
+then
+    echo "drop database $DATABASE_NAME;" | mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle
+else
+    echo "drop database $DATABASE_NAME;" | mysql -u$1 -p$2 --port=$3 --host=$4
+fi
+
+if [ $? -ne 0 ]; then
+    printf "Error attempting to connect to DB instance at $4:$3\n"
+    exit 1;
+fi
+
+##
+if [ $5 ]
+then
+    cat $SCRIPT_DIR/$DUMMY_DB_FILENAME | mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle
+else
+    cat $SCRIPT_DIR/$DUMMY_DB_FILENAME | mysql -u$1 -p$2 --port=$3 --host=$4
+fi
+
+if [ $? -ne 0 ]; then
+    printf "Error attempting to connect to DB instance at $4:$3\n"
+    exit 1;
+fi
+
+##
+if [ $5 ]
+then
+    NUM_STUDENTS=$(mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle -se "select count(*) from $DATABASE_NAME.student")
+else
+    NUM_STUDENTS=$(mysql -u$1 -p$2 --port=$3 --host=$4 -se "select count(*) from $DATABASE_NAME.student")
+fi
+
+if [ $NUM_STUDENTS -gt 499 ]; then
+    printf "At least 500 students in sample data set. All is well.\n"
+else 
+    printf "ERROR! More than 500 students expected in sample dataset...\n"
+fi
+
