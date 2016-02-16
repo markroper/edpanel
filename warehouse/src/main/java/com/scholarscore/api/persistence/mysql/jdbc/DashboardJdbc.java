5015f1f0ce343a116b6a711eec39f20265fedb90
markroper <markroper@gmail.com>
2016-02-16 11:56:05 -0500

    Adds implementations to the Dashboard persistence and manager classes.

--- warehouse/src/main/java/com/scholarscore/api/persistence/mysql/jdbc/DashboardJdbc.java
+++ warehouse/src/main/java/com/scholarscore/api/persistence/mysql/jdbc/DashboardJdbc.java
@@ -6,11 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * Created by markroper on 2/16/16.
  */
 @Transactional
 public class DashboardJdbc implements DashboardPersistence {
+    public static final String DASH_HQL = "from dashboard d " +
+            "join fetch rows r " +
+            "join fetch r.reports rs " +
+            "join fetch rs.chartQuery " +
+            "join fetch rs.clickTableQuery";
     @Autowired
     private HibernateTemplate hibernateTemplate;
 
@@ -27,26 +34,44 @@ public class DashboardJdbc implements DashboardPersistence {
 
     @Override
     public Long insertDashboard(Dashboard dashboard) {
-        return null;
+        dashboard.setId(null);
+        Dashboard d =  hibernateTemplate.merge(dashboard);
+        return d.getId();
     }
 
     @Override
-    public Dashboard selectDashboard(long schoolId, long dashboardId) {
-        return null;
+    public Dashboard selectDashboard(Long schoolId, Long dashboardId) {
+        return hibernateTemplate.get(Dashboard.class, dashboardId);
     }
 
     @Override
-    public Dashboard selectDashboardForUser(long schoolId, long userId) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public Dashboard selectDashboardForUser(Long schoolId, Long userId) {
+        String[] params = new String[]{ "userId", "schoolId" };
+        Object[] paramVals = new Object[]{ userId, schoolId };
+        List<Dashboard> dashes = (List<Dashboard>)hibernateTemplate.findByNamedParam(
+                DASH_HQL + " where d.userId = :userId and d.schoolId = :schoolId",
+                params,
+                paramVals);
+        if(null == dashes || dashes.isEmpty()) {
+            return null;
+        }
+        return dashes.get(0);
     }
 
     @Override
-    public void deleteDashboard(long dashboardId) {
-
+    public void deleteDashboard(Long dashboardId) {
+        Dashboard d = selectDashboard(null, dashboardId);
+        if(null != d) {
+            hibernateTemplate.delete(d);
+        }
     }
 
     @Override
-    public void updateDashboard(long schoolId, long dashboardId, Dashboard dash) {
-
+    public void updateDashboard(Long schoolId, Long dashboardId, Dashboard dash) {
+        Dashboard d = selectDashboard(schoolId, dashboardId);
+        if(null != d) {
+            hibernateTemplate.merge(dash);
+        }
     }
 }
