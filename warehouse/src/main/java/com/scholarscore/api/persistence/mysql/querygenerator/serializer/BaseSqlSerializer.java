d97c762d7e154ed982e1eafc50a09ad7f7441741
jordan <jodamn@gmail.com>
2016-02-17 13:20:31 -0500

    refactoring waypoint -- all appears to be working

--- warehouse/src/main/java/com/scholarscore/api/persistence/mysql/querygenerator/serializer/BaseSqlSerializer.java
+++ warehouse/src/main/java/com/scholarscore/api/persistence/mysql/querygenerator/serializer/BaseSqlSerializer.java
@@ -17,6 +17,25 @@ public abstract class BaseSqlSerializer implements MeasureSqlSerializer {
         return LEFT_OUTER_JOIN + toTableName() + ON +
                 dimTableName + DOT + QuerySqlGenerator.resolvePrimaryKeyField(dimTableName) + EQUALS +
                 toTableName() + DOT + dimTableName + FK_COL_SUFFIX + " "; // + sectionGradeJoin();
-        // TODO Jordan refactoring in progress...
+        // TODO Jordan refactoring in progress - many sqlizers include another join here. currently they must override toJoinClause and add it.
+    }
+
+    /* 
+* Generic method to join to a table
+* */
+    protected String joinTable(String tableToJoin) {
+        String inferredJoinFromColumnName = tableToJoin + FK_COL_SUFFIX;
+        String inferredJoinToColumnName = tableToJoin + ID_COL_SUFFIX;
+        return joinTable(tableToJoin, inferredJoinFromColumnName, inferredJoinToColumnName);
+    }
+
+    /* 
+    * Overloaded method to join tables allowing the foreign key columns to be specified.
+    * This should generally not be needed if the keys are named following the _id and _fk convention.
+    * */
+    protected String joinTable(String tableToJoin, String joinedFromTableFkName, String joinedToTableFkName) {
+        return LEFT_OUTER_JOIN + tableToJoin + ON +
+                toTableName() + DOT + joinedFromTableFkName +
+                EQUALS + tableToJoin + DOT + joinedToTableFkName + " ";
     }
 }
