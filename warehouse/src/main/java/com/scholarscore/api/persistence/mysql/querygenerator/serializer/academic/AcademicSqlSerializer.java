452ca50f4b77300bfc1d3cd75c9a138238b96b3c
jordan <jodamn@gmail.com>
2016-02-18 20:44:19 -0500

    intermediate refactoring -- adding optional join table scaffolding

---
+++ warehouse/src/main/java/com/scholarscore/api/persistence/mysql/querygenerator/serializer/academic/AcademicSqlSerializer.java
@@ -0,0 +1,14 @@
+package com.scholarscore.api.persistence.mysql.querygenerator.serializer.academic;
+
+import com.scholarscore.api.persistence.mysql.querygenerator.serializer.BaseSqlSerializer;
+
+/**
+ * User: jordan
+ * Date: 2/18/16
+ * Time: 8:14 PM
+ */
+public abstract class AcademicSqlSerializer extends BaseSqlSerializer {
+    
+    
+    
+}
