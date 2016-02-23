0603c83b0669c9181ff6b3514f4e6ab0e8c83c40
jordan <jodamn@gmail.com>
2016-02-22 22:44:35 -0500

    moving one Measure test to common, and adding one Dimension test

---
+++ common/src/test/java/com/scholarscore/models/query/measure/MeasureUnitTest.java
@@ -0,0 +1,24 @@
+package com.scholarscore.models.query.measure;
+
+import com.scholarscore.models.query.Measure;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * User: jordan
+ * Date: 2/22/16
+ * Time: 10:37 PM
+ */
+@Test(groups = { "unit" })
+public class MeasureUnitTest {
+
+    @Test
+    public void testMeasureHasClass() {
+        for (Measure measure : Measure.values()) {
+            IMeasure measureClass = Measure.buildMeasure(measure);
+            assertNotNull(measureClass, "Measure " + measure + " is defined in Measures Enum but Measure.buildMeasure produces null!");
+        }
+    }
+
+}
