e38f83f08b65adf7c5f2b7797e9dc4ff30a72e75
jordan <jodamn@gmail.com>
2016-02-28 21:55:40 -0500

    adding additional dimensions

---
+++ common/src/main/java/com/scholarscore/models/query/dimension/GpaDimension.java
@@ -0,0 +1,44 @@
+package com.scholarscore.models.query.dimension;
+
+import com.google.common.collect.ImmutableSet;
+import com.scholarscore.models.attendance.Attendance;
+import com.scholarscore.models.gpa.Gpa;
+import com.scholarscore.models.query.Dimension;
+
+import java.util.Set;
+
+/**
+ * User: jordan
+ * Date: 2/16/16
+ * Time: 5:15 PM
+ */
+public class GpaDimension implements IDimension {
+
+    public static final Set<Dimension> PARENT_DIMENSIONS =
+            ImmutableSet.of(Dimension.STUDENT);
+
+    @Override
+    public Dimension getType() {
+        return Dimension.GPA;
+    }
+
+    @Override
+    public Class<?> getAssociatedClass() {
+        return Gpa.class;
+    }
+
+    @Override
+    public Set<Dimension> getParentDimensions() {
+        return PARENT_DIMENSIONS;
+    }
+
+    @Override
+    public Set<String> getFields() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+}
