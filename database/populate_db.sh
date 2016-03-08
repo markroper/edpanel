d528dcec180e3b30ab959f656604d39130811f4e
jordan <jodamn@gmail.com>
2016-03-07 22:38:48 -0500

    name still hardcoded, but much duplication removed from populate_db.sh

--- database/populate_db.sh
+++ database/populate_db.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DUMMY_DB_FILENAME=dev_scholar_warehouse.sql
-DATABASE_NAME=@databasename
+DATABASE_NAME=scholar_warehouse
 
 if [ ! -f "$SCRIPT_DIR/$DUMMY_DB_FILENAME" ]; then
     printf "Dummy DB population FAILED - Dummy DB file does not exist at $SCRIPT_DIR/$DUMMY_DB_FILENAME..."
@@ -12,37 +12,27 @@ fi
 
 ## the fifth argument enables SSL
 if [ $5 ]
-then
-    echo "drop database $DATABASE_NAME;" | mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle
-else
-    echo "drop database $DATABASE_NAME;" | mysql -u$1 -p$2 --port=$3 --host=$4
+then 
+    MYSQL_COMMAND="mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle"
+else 
+    MYSQL_COMMAND="mysql -u$1 -p$2 --port=$3 --host=$4"
 fi
+
+echo "drop database if exists $DATABASE_NAME;" | ${MYSQL_COMMAND}
 
 if [ $? -ne 0 ]; then
     printf "Error attempting to connect to DB instance at $4:$3\n"
     exit 1;
 fi
 
-##
-if [ $5 ]
-then
-    cat $SCRIPT_DIR/$DUMMY_DB_FILENAME | mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle
-else
-    cat $SCRIPT_DIR/$DUMMY_DB_FILENAME | mysql -u$1 -p$2 --port=$3 --host=$4
-fi
+cat $SCRIPT_DIR/$DUMMY_DB_FILENAME | ${MYSQL_COMMAND}
 
 if [ $? -ne 0 ]; then
     printf "Error attempting to connect to DB instance at $4:$3\n"
     exit 1;
 fi
 
-##
-if [ $5 ]
-then
-    NUM_STUDENTS=$(mysql -u$1 -p$2 --port=$3 --host=$4 --ssl-ca=$SCRIPT_DIR/../warehouse/src/main/resources/db-public-key.ca-bundle -se "select count(*) from $DATABASE_NAME.student")
-else
-    NUM_STUDENTS=$(mysql -u$1 -p$2 --port=$3 --host=$4 -se "select count(*) from $DATABASE_NAME.student")
-fi
+NUM_STUDENTS=$(${MYSQL_COMMAND} -se "select count(*) from $DATABASE_NAME.student")
 
 if [ $NUM_STUDENTS -gt 499 ]; then
     printf "At least 500 students in sample data set. All is well.\n"
