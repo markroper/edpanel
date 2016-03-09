0dbb797cbf906f5a08e01d5ff0f44defc52c4ffd
jordan <jodamn@gmail.com>
2016-03-08 21:15:28 -0500

    making database scripts take name parameter

---
+++ database/dropAndRecreateDatabase.sql
@@ -0,0 +1,16 @@
+-- use a default DB name, but allow this to be overridden if @databasename was set earlier 
+set @default_database_name='scholar_warehouse';
+-- set @databasename=if(@databasename is not null, @databasename, @default_database_name)
+set @databasename=@default_database_name;
+
+SET @query = CONCAT('DROP DATABASE IF EXISTS ', @databasename, ';');
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+set @query = NULL;
+
+SET @query = CONCAT('CREATE DATABASE ', @databasename, ';');
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+set @query = NULL;
