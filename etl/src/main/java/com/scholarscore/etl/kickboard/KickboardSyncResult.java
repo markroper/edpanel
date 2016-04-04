578b0f1306c362b341ea4bc208720dd8d2a5ade5
markroper <markroper@gmail.com>
2016-04-04 07:05:45 -0400

    Adds tracking for point bank to the KickboardSyncResult

--- etl/src/main/java/com/scholarscore/etl/kickboard/KickboardSyncResult.java
+++ etl/src/main/java/com/scholarscore/etl/kickboard/KickboardSyncResult.java
@@ -10,9 +10,17 @@ public class KickboardSyncResult extends BaseSyncResult {
     int updated = 0;
     int deleted = 0;
 
+    int scoresCreated = 0;
+    int scoresUpdated = 0;
+    int scoresDeleted = 0;
+
     int failedCreated = 0;
     int failedUpdated = 0;
     int failedDeleted = 0;
+
+    int failedScoresCreated = 0;
+    int failedScoresUpdated = 0;
+    int failedScoresDeleted = 0;
 
     public void addCreated(int num) {
         created += num;
@@ -24,6 +32,18 @@ public class KickboardSyncResult extends BaseSyncResult {
 
     public void addDeleted(int num) {
         deleted += num;
+    }
+
+    public void addScoreCreated(int num) {
+        scoresCreated += num;
+    }
+
+    public void addScoreUpdated(int num) {
+        scoresUpdated += num;
+    }
+
+    public void addScoreDeleted(int num) {
+        scoresDeleted += num;
     }
 
     public void addFailedToCreate(int num) {
@@ -38,15 +58,35 @@ public class KickboardSyncResult extends BaseSyncResult {
         failedDeleted += num;
     }
 
+    public void addFailedScoreCreate(int num) {
+        failedScoresCreated += num;
+    }
+
+    public void addFailedScoreUpdate(int num) {
+        failedScoresUpdated += num;
+    }
+
+    public void addFailedScoreDeleted(int num) {
+        failedScoresDeleted += num;
+    }
+
     @Override
     public String getResultString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Behavior events created: " + created + "\n");
-        builder.append("Behavior events udpated: " + updated + "\n");
+        builder.append("Behavior events updated: " + updated + "\n");
         builder.append("Behavior events deleted: " + deleted + "\n");
+        builder.append("Behavior scores created: " + scoresCreated + "\n");
+        builder.append("Behavior scores updated: " + scoresUpdated + "\n");
+        builder.append("Behavior scores deleted: " + scoresDeleted + "\n");
+
         builder.append("Behavior event failed creates: " + failedCreated + "\n");
         builder.append("Behavior event failed updates: " + failedUpdated + "\n");
         builder.append("Behavior event failed deleted: " + failedDeleted + "\n");
+        builder.append("Behavior score failed creates: " + failedScoresCreated + "\n");
+        builder.append("Behavior score failed updates: " + failedScoresUpdated + "\n");
+        builder.append("Behavior score failed deletes: " + failedScoresDeleted + "\n");
+
         return builder.toString();
     }
 }
