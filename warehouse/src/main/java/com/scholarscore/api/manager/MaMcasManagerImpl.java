69e71085baedc677b44de8a9ed580f501dfc0d18
markroper <markroper@gmail.com>
2016-04-10 12:36:07 -0400

    Adds the API and hibernate JDBC mapper classes.  Up next: API integration tests.

---
+++ warehouse/src/main/java/com/scholarscore/api/manager/MaMcasManagerImpl.java
@@ -0,0 +1,63 @@
+package com.scholarscore.api.manager;
+
+import com.scholarscore.api.persistence.MaMcasPersistence;
+import com.scholarscore.api.util.ServiceResponse;
+import com.scholarscore.models.state.ma.McasResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by markroper on 4/10/16.
+ */
+public class MaMcasManagerImpl implements MaMcasManager {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MaMcasManagerImpl.class);
+
+    @Autowired
+    private MaMcasPersistence mcasPersistence;
+
+    @Autowired
+    private OrchestrationManager pm;
+
+    public void setMcasPersistence(MaMcasPersistence mcasPersistence) {
+        this.mcasPersistence = mcasPersistence;
+    }
+
+    public void setPm(OrchestrationManager pm) {
+        this.pm = pm;
+    }
+
+    @Override
+    public ServiceResponse<List<McasResult>> getAllMcasResultsForStudent(Long schoolId, Long studentId) {
+        return new ServiceResponse<>(mcasPersistence.selectMcasForStudent(schoolId, studentId));
+    }
+
+    @Override
+    public ServiceResponse<McasResult> getMcasResult(Long schoolId, Long studentId, Long mcasId) {
+        return new ServiceResponse<>(mcasPersistence.select(mcasId));
+    }
+
+    @Override
+    public ServiceResponse<Long> createMcasResult(Long schoolId, Long studentId, McasResult result) {
+        return new ServiceResponse<>(mcasPersistence.insertMcasResult(schoolId, studentId, result));
+    }
+
+    @Override
+    public ServiceResponse<List<Long>> createMcasResults(List<McasResult> results) {
+        return new ServiceResponse<>(mcasPersistence.insertMcasResults(results));
+    }
+
+    @Override
+    public ServiceResponse<Void> replaceMcasResult(Long schoolId, Long studentId, Long mcasId, McasResult result) {
+        mcasPersistence.replaceMcasResult(schoolId, studentId, mcasId, result);
+        return new ServiceResponse<>((Void) null);
+    }
+
+    @Override
+    public ServiceResponse<Void> deleteMcasResult(Long schoolId, Long studentId, Long mcasId) {
+        mcasPersistence.deleteMcasResult(schoolId, studentId, mcasId);
+        return new ServiceResponse<>((Void) null);
+    }
+}
