58c5b8fb6b5df90ee1afa634e01e1e35bedb2955
markroper <markroper@gmail.com>
2016-04-14 14:18:47 -0400

    Updates parser base class.

--- etl/src/main/java/com/scholarscore/etl/schoolbrains/BaseParser.java
+++ etl/src/main/java/com/scholarscore/etl/schoolbrains/BaseParser.java
@@ -1,16 +1,44 @@
 package com.scholarscore.etl.schoolbrains;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by markroper on 4/14/16.
  */
 public abstract class BaseParser<T> {
+    private final static Logger LOGGER = LoggerFactory.getLogger(BaseParser.class);
     protected File input;
+    protected CSVParser parser;
     public BaseParser(File file) {
         this.input = file;
     }
 
-    public abstract List<T> parse();
+    public abstract T parseRec(CSVRecord rec);
+
+    public List<T> parse() {
+        if(null == input) {
+            return null;
+        }
+        try {
+            List<T> results = new ArrayList<>();
+            CSVParser parser = CSVParser.parse(input, Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
+            for (CSVRecord rec : parser) {
+               results.add(parseRec(rec));
+            }
+            return results;
+        } catch (IOException e) {
+            LOGGER.error("Unable to parse CSV file. " + e.getMessage());
+            return null;
+        }
+    }
 }
--- etl/src/main/java/com/scholarscore/etl/schoolbrains/StudentParser.java
+++ etl/src/main/java/com/scholarscore/etl/schoolbrains/StudentParser.java
@@ -1,17 +1,11 @@
 package com.scholarscore.etl.schoolbrains;
 
 import com.scholarscore.models.user.Student;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by markroper on 4/14/16.
@@ -22,21 +16,37 @@ public class StudentParser extends BaseParser<Student> {
         super(f);
     }
 
-    @Override
-    public List<Student> parse() {
-        if(null == input) {
-            return null;
-        }
-        try {
-            List<Student> results = new ArrayList<>();
-            CSVParser parser = CSVParser.parse(input, Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
-            for (CSVRecord rec : parser) {
+    private static final int SASID = 0;
+    private static final int  STUDENTID = 1;
+    private static final int GRADEID = 2;
+    private static final int CURRENTGRADEID = 3;
+    private static final int CURRENTSCHOOL = 4;
+    private static final int DATE_OF_BIRTH = 5;
+    private static final int EMAIL = 6;
+    private static final int ETHNIC_CATEGORY = 7;
+    private static final int ETHNIC_CODE = 8;
+    private static final int ETHNIC_ID = 9;
+    private static final int FIRST_NAME = 10;
+    private static final int MIDDlE_NAME = 11;
+    private static final int LAST_NAME = 12;
+    private static final int GENDER = 13;
+    private static final int HOME_ADDRESS_LINE_1 = 14;
+    private static final int HOME_ADDRESS_LINE_2 = 15;
+    private static final int HOME_CITY = 16;
+    private static final int HOME_STATE_ID = 17;
+    private static final int HOME_POSTAL_CODE = 18;
+    private static final int LASID = 19;
+    private static final int SCHOOL_STATE_ID = 20;
+    private static final int YEAR_OF_GRADUATION = 21;
+    private static final int SCHOOL_ID = 22;
+    private static final int SPECIAL_ED_EVAL_RESULTS = 23;
+    private static final int SPECIAL_ED_LEVEL_OF_NEED = 24;
+    private static final int ELL_STATUS = 25;
 
-            }
-            return results;
-        } catch (IOException e) {
-            LOGGER.error("Unable to parse MCAS file. " + e.getMessage());
-            return null;
-        }
+    @Override
+    public Student parseRec(CSVRecord rec) {
+        Student s = new Student();
+        s.setStateStudentId(rec.get(SASID));
+        return s;
     }
 }
